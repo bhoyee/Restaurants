cdfe16ca4db22f257f1ed55bb23c59a84ded5d60
unknown <s.adeboye@outlook.com>
2024-06-08 16:52:50 +0100

    init first migration

--- Restaurants.API/obj/Debug/net8.0/Restaurants.API.AssemblyInfo.cs
+++ Restaurants.API/obj/Debug/net8.0/Restaurants.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurants.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de2331e24e8be25a30eca83363a239105602dfd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c98b8f495e9ffcc537859d29c1749e001818d79")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurants.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurants.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Restaurants.Infrastructure/obj/Debug/net8.0/Restaurants.Infrastructure.AssemblyInfo.cs
+++ Restaurants.Infrastructure/obj/Debug/net8.0/Restaurants.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurants.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de2331e24e8be25a30eca83363a239105602dfd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c98b8f495e9ffcc537859d29c1749e001818d79")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurants.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurants.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
