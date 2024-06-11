8363a7e38ffb088628882849995987c8780624f5
unknown <s.adeboye@outlook.com>
2024-06-11 22:10:11 +0100

    Implement DTOs to the application

--- Restaurants.Application/Restaurants/RestaurantsService.cs
+++ Restaurants.Application/Restaurants/RestaurantsService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Restaurants.Application.Dtos;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
 using System;
@@ -12,18 +13,22 @@ namespace Restaurants.Application.Restaurants
     internal class RestaurantsService(IRestaurantsRepository restaurantsRepository, 
         ILogger<RestaurantsService> logger) : IRestaurantsService
     {
-        public async Task<IEnumerable<Restaurant>> GetAllRestaurants()
+        public async Task<IEnumerable<RestaurantDto>> GetAllRestaurants()
         {
             logger.LogInformation("Get all restaurants");
             var restaurants = await restaurantsRepository.GetAllAsync();
-            return restaurants;
+
+            var restaurantDtos = restaurants.Select(RestaurantDto.FromEntity);
+
+            return restaurantDtos!;
         }
 
-        public async Task<Restaurant?> GetById(int id)
+        public async Task<RestaurantDto?> GetById(int id)
         {
             logger.LogInformation($"Getting restaurant {id}");
             var restaurant = await restaurantsRepository.GetByIdAsync(id);
-            return restaurant;
+            var restaurantDtos = RestaurantDto.FromEntity(restaurant);
+            return restaurantDtos;
         }
     }
 }
