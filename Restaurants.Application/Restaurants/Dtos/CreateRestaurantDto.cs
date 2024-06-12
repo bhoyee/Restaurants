2427ecce1b65a9940aae629530eedf44217ac7be
unknown <s.adeboye@outlook.com>
2024-06-12 22:17:27 +0100

    updated createRestaurantDto

--- Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
+++ Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,24 @@ namespace Restaurants.Application.Restaurants.Dtos
 {
     public class CreateRestaurantDto
     {
+        [StringLength(100, MinimumLength = 3)]
         public string Name { get; set; } = default!;
         public string Description { get; set; } = default!;
+
+        [Required(ErrorMessage = "Insert a valid category")]
         public string Category { get; set; } = default!;
         public bool HasDelivery { get; set; }
 
+        [EmailAddress(ErrorMessage = "Please provide valid email address")]
         public string? ContactEmail { get; set; }
+
+        [Phone(ErrorMessage = "Please provide valid phone number")]
         public string? ContactNumber { get; set; }
 
         public string? City { get; set; }
         public string? Street { get; set; }
+
+        [RegularExpression(@"^\d{2}-\d{3}$", ErrorMessage = "Please provide a valid postal code (XX-XXX).")]
         public string? PostalCode { get; set; }
     }
 }
