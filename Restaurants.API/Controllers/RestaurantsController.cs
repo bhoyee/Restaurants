9bb379fb6cd585c4fbaff92c361cd5d1b753eca9
unknown <s.adeboye@outlook.com>
2024-06-12 22:18:22 +0100

    updated the RestaurantController

--- Restaurants.API/Controllers/RestaurantsController.cs
+++ Restaurants.API/Controllers/RestaurantsController.cs
@@ -30,6 +30,8 @@ namespace Restaurants.API.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateRestaurant([FromBody]CreateRestaurantDto createRestaurantDto)
         {
+            if(!ModelState.IsValid) return BadRequest(ModelState);
+
             int id = await restaurantsService.Create(createRestaurantDto);
             return CreatedAtAction(nameof(GetById), new { id }, null);
         }
