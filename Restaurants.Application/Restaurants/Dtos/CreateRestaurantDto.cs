7b20a3787492984056542660892729c6efe51bcd
unknown <s.adeboye@outlook.com>
2024-06-15 19:02:04 +0100

    implemented fluentvalidation on create restaurant

--- Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
+++ Restaurants.Application/Restaurants/Dtos/CreateRestaurantDto.cs
@@ -9,24 +9,24 @@ namespace Restaurants.Application.Restaurants.Dtos
 {
     public class CreateRestaurantDto
     {
-        [StringLength(100, MinimumLength = 3)]
+        //[StringLength(100, MinimumLength = 3)]
         public string Name { get; set; } = default!;
         public string Description { get; set; } = default!;
 
-        [Required(ErrorMessage = "Insert a valid category")]
+        //[Required(ErrorMessage = "Insert a valid category")]
         public string Category { get; set; } = default!;
         public bool HasDelivery { get; set; }
 
-        [EmailAddress(ErrorMessage = "Please provide valid email address")]
+        //[EmailAddress(ErrorMessage = "Please provide valid email address")]
         public string? ContactEmail { get; set; }
 
-        [Phone(ErrorMessage = "Please provide valid phone number")]
+        //[Phone(ErrorMessage = "Please provide valid phone number")]
         public string? ContactNumber { get; set; }
 
         public string? City { get; set; }
         public string? Street { get; set; }
 
-        [RegularExpression(@"^\d{2}-\d{3}$", ErrorMessage = "Please provide a valid postal code (XX-XXX).")]
+        //[RegularExpression(@"^\d{2}-\d{3}$", ErrorMessage = "Please provide a valid postal code (XX-XXX).")]
         public string? PostalCode { get; set; }
     }
 }
---
+++ Restaurants.Application/Restaurants/Validators/CreateResturantDtoValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using Restaurants.Application.Restaurants.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Application.Restaurants.Validators
+{
+    public class CreateResturantDtoValidator : AbstractValidator<CreateRestaurantDto>
+    {
+        public CreateResturantDtoValidator()
+        {
+            RuleFor(dto => dto.Name)
+                .Length(3, 100);
+
+            RuleFor(dto => dto.Description)
+                .NotEmpty().WithMessage("Description is required.");
+
+            RuleFor(dto => dto.Category)
+                .NotEmpty().WithMessage("Insert a valid category");
+
+            RuleFor(dto => dto.ContactEmail)
+                .EmailAddress()
+                .WithMessage("Please provide a valid email address");
+
+            RuleFor(dto => dto.PostalCode)
+                .Matches(@"^\d{2}-\d{3}$")
+                .WithMessage("Please provide a valid postal code (xx-xxx).");
+
+        }
+    }
+}
