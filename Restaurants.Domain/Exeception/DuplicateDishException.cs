035baf06ba8a15c5ba7d62fd8203513e80884343
unknown <s.adeboye@outlook.com>
2024-06-25 23:40:47 +0100

    add duplicateDishExeception

---
+++ Restaurants.Domain/Exeception/DuplicateDishException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Domain.Exeception
+{
+    public class DuplicateDishException : Exception
+    {
+        public DuplicateDishException(string dishName, int restaurantId)
+            : base($"A dish with name '{dishName}' already exists for restaurant with ID {restaurantId}.")
+        {
+        }
+    }
+
+}
